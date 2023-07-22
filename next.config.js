927578fd6620cb7ef73f1e30d74d7f113dd13355
Kyaw Thet Paing <ikyawthetpaing@gmail.com>
2023-07-22 21:38:39 +0700

    Deployment update.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+await import("./src/env.mjs");
+
 const nextConfig = {
   images: {
     domains: ["fakeimg.pl", "picsum.photos", "uploadthing.com"],
-  }
+  },
 };
 
 module.exports = nextConfig;
--- src/env.mjs
+++ src/env.mjs
@@ -11,6 +11,8 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     DATABASE_URL: z.string().min(1),
     ADMIN_EMAIL: z.string().min(1),
+    UPLOADTHING_APP_ID: z.string().min(1),
+    UPLOADTHING_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
@@ -23,5 +25,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
   },
 });
